b05f04f2a89dd107068b87c210af67c126b9ab40
liangwei0101 <liangwei0101@users.noreply.github.com>
2018-08-22 20:13:27 +0800

    增加打包的相关代码

--- src/main/java/com/hundsun/book/BookApplication.java
+++ src/main/java/com/hundsun/book/BookApplication.java
@@ -3,13 +3,20 @@ package com.hundsun.book;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
 @EnableSwagger2
 @SpringBootApplication
 @MapperScan("com.hundsun.book.mapper")
-public class BookApplication {
+public class BookApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(BookApplication.class);
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookApplication.class, args);
--- src/main/java/com/hundsun/book/config/CorsConfig.java
+++ src/main/java/com/hundsun/book/config/CorsConfig.java
@@ -1,6 +1,5 @@
 package com.hundsun.book.config;
 
-import org.apache.catalina.filters.CorsFilter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
